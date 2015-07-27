2a5a421e8e31294e8b038b584bf72d496f70049c
dnrocha1 <dn.rocha@outlook.com>
2015-07-27 17:57:06 -0300

    EasyAccept

---
+++ ProjetoP2/src/core/popPlusTestClient.java
@@ -0,0 +1,25 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import easyaccept.EasyAcceptFacade;
+
+public class popPlusTestClient {
+
+	public static void main(String[] args) {
+
+		List<String> files = new ArrayList<String>();
+		// Put the us1.txt file into the "test scripts" list
+		files.add("use_case_1.txt");
+		// Instantiate the Monopoly Game façade
+		Facade popFacade = new Facade();
+		// Instantiate EasyAccept façade
+		EasyAcceptFacade eaFacade = new EasyAcceptFacade(popFacade, files);
+		// Execute the tests
+		eaFacade.executeTests();
+		// Print the tests execution results
+		System.out.println(eaFacade.getCompleteResults());
+	}
+
+}
