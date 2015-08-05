0529561718db7a6bacb1199919afe86e17fb8420
dnrocha1 <dn.rocha@outlook.com>
2015-08-05 16:35:17 -0300

    asdf

--- ProjetoP2/src/core/Postagem.java
+++ ProjetoP2/src/core/Postagem.java
@@ -6,33 +6,33 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class Postagem {
-	private String conteudo;
-	private String dataDePostagem;
+	private String mensagem;
+	private String data;
 	private int pontosPopularidade;
-	ArrayList<String> hastags = new ArrayList<String>();
+	private ArrayList<String> tags = new ArrayList<String>();
 	private int likes;
 	private int deslikes;
 
 	public Postagem(String conteudo, String data) {
-		this.conteudo = conteudo;
-		this.dataDePostagem = data;
+		this.mensagem = conteudo;
+		this.data = data;
 		this.pontosPopularidade = 0;
 		this.likes = 0;
 		this.deslikes = 0;
-		this.hastags.addAll(procuraHastags(conteudo));
+		this.tags.addAll(procuraHastags(conteudo));
 	}
 
 	private Collection<? extends String> procuraHastags(String conteudo2) {
-		return hastags;
+		return tags;
 		// num sei
 	}
 	
 	public String getConteudo(){
-		return conteudo;
+		return mensagem;
 	}
 	
 	public String getData() throws Exception{
-		String data = this.dataDePostagem;
+		String data = this.data;
 		SimpleDateFormat input = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		SimpleDateFormat myFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String output = myFormat.format(input.parse(data));
