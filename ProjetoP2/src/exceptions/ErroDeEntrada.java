890c4044f22568896e0cf21023a0298983574aee
Matheus <Matheus@192.168.25.5>
2015-11-20 01:33:14 -0300

    Erros

---
+++ ProjetoP2/src/exceptions/ErroDeEntrada.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class ErroDeEntrada extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ErroDeEntrada(String string) {
+		super(string);
+	}
+
+}
---
+++ ProjetoP2/src/exceptions/ErroDeLogica.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class ErroDeLogica extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ErroDeLogica(String string) {
+		super(string);
+	}
+
+}
